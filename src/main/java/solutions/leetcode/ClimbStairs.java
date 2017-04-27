545e893e1549e96d3990e384c19123e0314dda0b
Chris Li <hongyuanChrisLi@users.noreply.github.com>
2017-04-27 11:54:19 -0700

    Climb Stairs

--- src/main/java/solutions/leetcode/ClimbStairs.java
+++ src/main/java/solutions/leetcode/ClimbStairs.java
@@ -4,6 +4,19 @@ import static org.junit.Assert.assertEquals;
 
 import base.Solution;
 
+/*
+ * 
+https://leetcode.com/submissions/detail/101366508/
+
+Fibonacci numbers at its core
+
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+
+ * 
+ * */
+
 public class ClimbStairs extends Solution<Integer, Integer> {
 
     @Override
