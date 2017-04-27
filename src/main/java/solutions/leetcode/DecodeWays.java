28a6016e91453716a4b1590464f5c685266eb065
Chris Li <hongyuanChrisLi@users.noreply.github.com>
2017-04-27 15:42:11 -0700

    Decode ways

--- src/main/java/solutions/leetcode/DecodeWays.java
+++ src/main/java/solutions/leetcode/DecodeWays.java
@@ -4,6 +4,26 @@ import static org.junit.Assert.assertEquals;
 
 import base.Solution;
 
+/*
+ * 
+https://leetcode.com/problems/decode-ways/#/description
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+
+ * 
+ * */
+
 public class DecodeWays extends Solution<String, Integer> {
 
     @Override
@@ -22,7 +42,32 @@ public class DecodeWays extends Solution<String, Integer> {
     }
     
     public int numDecodings(String s) {
-        return 0;
+        
+        int strLen = s.length();
+        if (strLen == 0 || s.charAt(0) == '0') return 0;
+        int[] ref = new int[strLen + 1];
+        ref[0] = 1;
+        ref[1] = 1;
+        char preVal = s.charAt(0);
+                
+        for ( int i = 2; i <= strLen; i++ ){
+            char curVal = s.charAt(i-1);
+            if ( curVal == '0') {
+                if (preVal == '1' || preVal == '2')         // case "...10" or "...20"
+                    if ( ref[i-1] > ref[i-2])               // case "...210", "..220"...
+                        ref[i] = ref[i-2];
+                    else                                    // case "...420" ...
+                        ref[i] = ref[i-1];
+                else return 0;                              // case "...30" ...
+            } else {
+                if (preVal == '1' || ( preVal == '2' && curVal < '7'))
+                    ref[i] = ref[i-1] + ref[i-2];           // case "...18", "...24" ...    
+                else
+                    ref[i] = ref[i-1];                      // case "...61", "...29" ...
+            }
+            preVal = curVal;
+        }
+        return ref[strLen];
     }
 
 }
--- src/main/java/testcases/leetcode/DecodeWaysTestCases.java
+++ src/main/java/testcases/leetcode/DecodeWaysTestCases.java
@@ -10,6 +10,20 @@ public class DecodeWaysTestCases extends TestCases<String, Integer> implements T
 
     public DecodeWaysTestCases(){
         addTestCase("Test Example 1", "12", 2);
+        addTestCase("Test No.2", "12345", 3);
+        addTestCase("Test 0 Start", "012", 0);
+        addTestCase("Test 10", "10", 1);
+        addTestCase("Test 210", "210", 1);
+        addTestCase("Test 2101", "2101", 1);
+        addTestCase("Test 1310", "1310", 2);
+        addTestCase("Test Invalid 0", "130", 0);
+        addTestCase("Test 111", "111", 3);
+        addTestCase("Test 1111", "1111", 5);
+        addTestCase("Test 12121", "12121", 8);
+        addTestCase("Test 13121", "13121", 6);
+        addTestCase("Test 13127", "13127", 4);
+        addTestCase("Test 131210", "131210", 4);
+        addTestCase("Test 100", "100", 0);
     }
     
     @Override
