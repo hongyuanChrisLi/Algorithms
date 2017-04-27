e919bb0aa280cb5ea125bc6466897d67b0abe489
Chris Li <hongyuanChrisLi@users.noreply.github.com>
2017-04-27 10:39:21 -0700

    Min Path Sum

--- src/main/java/solutions/leetcode/MinPathSum.java
+++ src/main/java/solutions/leetcode/MinPathSum.java
@@ -2,7 +2,21 @@ package solutions.leetcode;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import base.Solution;
+
+
+/*
+ * 
+https://leetcode.com/problems/minimum-path-sum/#/description
+
+Given a m x n grid filled with non-negative numbers, 
+    find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+ * 
+ * */
 
 public class MinPathSum extends Solution<int[][], Integer> {
 
@@ -22,7 +36,29 @@ public class MinPathSum extends Solution<int[][], Integer> {
     }
     
     public int minPathSum(int[][] grid) {
-        return 0;
+        
+        int m = grid.length, n;
+        //System.out.println(m);
+        if (m > 0) n = grid[0].length;
+        else return 0;
+        
+        int[][] costTab = new int[m][n];
+        costTab[0][0] = grid[0][0];
+        
+        for (int i = 1; i < m; i++ )
+            costTab[i][0] = costTab[i-1][0] + grid[i][0];
+        
+        for (int j = 1; j < n; j++ )
+            costTab[0][j] = costTab[0][j-1] + grid[0][j];
+        
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++) 
+                costTab[i][j] = Math.min(costTab[i][j-1], costTab[i-1][j]) + grid[i][j];
+        
+        /*for (int i = 0; i < m; i++)
+            System.out.println(Arrays.toString(costTab[i]));*/
+        
+        return costTab[m-1][n-1];
     }
 
 }
--- src/main/java/testcases/leetcode/MinPathSumTestCases.java
+++ src/main/java/testcases/leetcode/MinPathSumTestCases.java
@@ -15,7 +15,15 @@ public class MinPathSumTestCases extends TestCases<int[][], Integer> implements 
             {4, 1, 3, 2, 2},
             {3, 1, 4, 1, 3},
             {4, 2, 3, 3, 2},
-            {3, 1, 1, 2, 1}}, 17);
+            {3, 1, 1, 2, 1}}, 16);
+        
+        addTestCase("Empty Input",new int[][]{}, 0);
+        addTestCase("Single Row", 
+                new int[][]{{4, 3, 2, 1, 1}}, 11);
+        addTestCase("Single Column", 
+                new int[][]{{4},{4},{3},{4},{3}}, 18);
+        addTestCase("Single Element", 
+                new int[][]{{2}}, 2);
     }
     
     @Override
