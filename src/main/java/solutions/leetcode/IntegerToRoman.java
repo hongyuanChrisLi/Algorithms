85c6cbc4a34b48b64461ab5e15f787e57704d67f
Chris Li <hongyuanChrisLi@users.noreply.github.com>
2017-05-04 19:47:50 -0700

    Integer to Roman

--- src/main/java/solutions/leetcode/IntegerToRoman.java
+++ src/main/java/solutions/leetcode/IntegerToRoman.java
@@ -4,6 +4,16 @@ import static org.junit.Assert.assertEquals;
 
 import base.Solution;
 
+/*
+ * 
+
+https://leetcode.com/problems/integer-to-roman/#/description
+Given an integer, convert it to a roman numeral.
+Input is guaranteed to be within the range from 1 to 3999.
+
+ * 
+ * */
+
 public class IntegerToRoman extends Solution<Integer, String> {
 
     @Override
@@ -22,6 +32,23 @@ public class IntegerToRoman extends Solution<Integer, String> {
     }
     
     public String intToRoman(int num) {
-        return "";
+        String[] baseRoman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] baseInt = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000, 4000};
+        //int baseLen = 13;
+        int sig = 0;
+        String res = "";
+        
+        while (num >= baseInt[sig]) sig++;
+
+        //System.out.println(baseInt[sig]);
+        
+        for ( int i = sig - 1; i >= 0; i-- ){
+            int fract = num / baseInt[i];
+            String part = "";
+            while(fract-- > 0) part += baseRoman[i];
+            res += part;
+            num = num  % baseInt[i];
+        }
+        return res;
     }
 }
--- src/main/java/testcases/leetcode/IntegerToRomanTestCases.java
+++ src/main/java/testcases/leetcode/IntegerToRomanTestCases.java
@@ -9,7 +9,25 @@ public class IntegerToRomanTestCases extends TestCases<Integer, String> implemen
     private static final long serialVersionUID = -8507563458149683533L;
 
     public IntegerToRomanTestCases(){
-        addTestCase("Test Basic", 5, "V");
+        addTestCase("Test 1", 1, "I");
+        addTestCase("Test 2", 2, "II");
+        addTestCase("Test 4", 4, "IV");
+        addTestCase("Test 5", 5, "V");
+        addTestCase("Test 7", 7, "VII");
+        addTestCase("Test 9", 9, "IX");
+        addTestCase("Test 10", 10, "X");
+        addTestCase("Test 15", 15, "XV");
+        addTestCase("Test 39", 39, "XXXIX");
+        addTestCase("Test 40", 40, "XL");
+        addTestCase("Test 49", 49, "XLIX");
+        addTestCase("Test 51", 51, "LI");
+        addTestCase("Test 3999", 3999, "MMMCMXCIX");
+        addTestCase("Test 1245", 1245, "MCCXLV");
+        addTestCase("Test 555", 555, "DLV");
+        addTestCase("Test 179", 179, "CLXXIX");
+        
+
+        
     }
     
     @Override
