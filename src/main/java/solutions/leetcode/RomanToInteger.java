6247cbc2f2e5f446695171db60d4604f655378cf
Chris Li <hongyuanChrisLi@users.noreply.github.com>
2017-05-06 14:16:36 -0700

    Roman to Integer

--- src/main/java/solutions/leetcode/RomanToInteger.java
+++ src/main/java/solutions/leetcode/RomanToInteger.java
@@ -21,8 +21,32 @@ public class RomanToInteger extends Solution<String, Integer> {
         assertEquals(outputTest, output);
     }
 
-    public int romanToInt(String s) {
-        return 0;
+    public int romanToInt(String s) {   
+        
+        int strLen = s.length();
+        if (strLen < 1) return 0;
+        s = s + (char) 'B';
+        /*
+         * 
+         Char   I   V   X   L   C   D   M
+         ASCII  73  86  88  76  67  68  77
+         -'B'   6   19  21  9   0   1   10  // ASCII - 66
+         Key    7   6   8   10  1   2   11  // %14
+         
+         * */
+        int[] sCharMap = new int[]{0, 100, 500, 0, 0, 0, 5, 1, 10, 0, 50, 1000};
+        int MVAL = 14;
+        
+        int res = sCharMap[(s.charAt(0) - 'B') % MVAL];
+        int preVal = res;
+        
+        
+        for (int i = 1; i < strLen; i++) {
+            int curVal= sCharMap[(s.charAt(i) - 'B') % MVAL];
+            res += curVal - (curVal > preVal ? preVal * 2 : 0);
+            preVal = curVal;
+        }
+        return res;
     }
     
 }
