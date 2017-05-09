69e9eea499066fe1ecb8ccfae3e5b57153fc604c
Chris Li <hongyuanChrisLi@users.noreply.github.com>
2017-05-09 16:05:13 -0700

    Valid brackets

--- src/main/java/solutions/leetcode/ValidBrackets.java
+++ src/main/java/solutions/leetcode/ValidBrackets.java
@@ -2,6 +2,8 @@ package solutions.leetcode;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.LinkedList;
+
 import base.Solution;
 import base.TestCases;
 import testcases.leetcode.ValidBracketsTestCases;
@@ -29,7 +31,26 @@ public class ValidBrackets extends Solution<String, Boolean> {
     }
     
     public boolean isValid(String s) {
-        return false;
+        
+        char[] lkp = new char[128];
+        lkp['}'] = '{';
+        lkp[')'] = '(';
+        lkp[']'] = '[';
+        //lkp['>'] = '<';
+        
+        LinkedList<Character> stack = new LinkedList<Character>();
+        
+        int len = s.length();
+        for(int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (lkp[c] == 0) 
+                stack.push(c);
+            else if (stack.isEmpty() || lkp[c] != stack.getFirst())
+                return false;
+            else stack.pop();
+        }
+        
+        return stack.isEmpty();
     }
 
 }
--- src/main/java/testcases/leetcode/ValidBracketsTestCases.java
+++ src/main/java/testcases/leetcode/ValidBracketsTestCases.java
@@ -4,6 +4,18 @@ import base.TestCases;
 import structures.TestCase;
 import structures.TestCaseHandler;
 
+/*
+ * 
+https://leetcode.com/problems/valid-parentheses/#/description
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+
+
+ * 
+ * */
+
 public class ValidBracketsTestCases extends TestCases<String, Boolean> implements TestCaseHandler<String, Boolean> {
 
     private static final long serialVersionUID = -7438834775966134992L;
@@ -11,6 +23,9 @@ public class ValidBracketsTestCases extends TestCases<String, Boolean> implement
     public ValidBracketsTestCases(){
         addTestCase("Test 1", "{}[({})]", true);
         addTestCase("Test 2", "{}[(})]", false);
+        addTestCase("Test 3", "", true);
+        addTestCase("Test 4", "[", false);
+        addTestCase("Test 4", "}", false);
     }
     
     @Override
